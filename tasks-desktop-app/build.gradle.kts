f269968e07854dd5edb2c95bd092c4c4b7c76918
Olivier Patry <olivier.patry@gmail.com>
2024-09-17 00:01:39 +0200

    Rename desktop app

--- tasks-desktop-app/build.gradle.kts
+++ tasks-desktop-app/build.gradle.kts
@@ -52,6 +52,14 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.material3)
             implementation(compose.components.resources)
+//            implementation(libs.androidx.compose.material3.adaptive)
+//            implementation(libs.androidx.compose.material3.adaptive.layout)
+//            implementation(libs.androidx.compose.material3.adaptive.navigation)
+//            implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+//            implementation("androidx.compose.material3:material3:1.3.0")
+//            implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
+//            implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0")
+//            implementation(libs.androidx.window.core)
         }
 
         jvmMain.dependencies {
