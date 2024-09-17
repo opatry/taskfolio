cf3b4286cc46c0c8158c29093fdfa03538818929
Olivier Patry <olivier.patry@gmail.com>
2024-09-17 11:29:35 +0200

    Navigation scaffold

--- tasks-desktop-app/src/commonMain/kotlin/net/opatry/tasks/app/TasksApp.kt
+++ tasks-desktop-app/src/commonMain/kotlin/net/opatry/tasks/app/TasksApp.kt
@@ -22,15 +22,35 @@
 
 package net.opatry.tasks.app
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import net.opatry.google.tasks.model.TaskList
+import ListTodo as LucideListTodo
+import Search as LucideSearch
+import Settings as LucideSettings
+
+enum class Destination(
+    val label: String,
+    val icon: ImageVector,
+    val contentDescription: String? = null,
+) {
+    Tasks("Account", LucideListTodo),
+    Search("Search", LucideSearch),
+    Settings("Settings", LucideSettings),
+}
 
 @Composable
 fun TasksApp(taskLists: List<TaskList>) {
@@ -44,16 +64,57 @@ fun TasksApp(taskLists: List<TaskList>) {
         // TODO scaffold + bottom bar vs nav rail WindowSizeClass
         // TODO where to put Theme+Surface
 
-        Column(Modifier.padding(innerPadding)) {
-            Text("Hello, Tasks!")
-
-            LazyColumn {
-                item(taskLists) {
-                    taskLists.forEach {
-                        Text(it.title)
+        val selectedItem = remember { mutableStateOf(Destination.Tasks) }
+        NavigationSuiteScaffold(navigationSuiteItems = {
+            Destination.entries.forEach {
+                item(
+                    selected = selectedItem.value == it,
+                    onClick = { selectedItem.value = it },
+                    label = { Text(it.label) },
+                    icon = {
+                        Icon(
+                            imageVector = it.icon,
+                            contentDescription = it.contentDescription
+                        )
+                    },
+                    alwaysShowLabel = true,
+                )
+            }
+        }) {
+            Box(Modifier.fillMaxSize()) {
+                Surface(Modifier.align(Alignment.Center)) {
+                    when (selectedItem.value) {
+                        Destination.Tasks -> TasksScreen(taskLists)
+                        Destination.Search -> SearchScreen()
+                        Destination.Settings -> SettingsScreen()
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun TasksScreen(taskLists: List<TaskList>) {
+    if (taskLists.isEmpty()) {
+        Text("No task list")
+    } else {
+        LazyColumn {
+            item(taskLists) {
+                taskLists.forEach {
+                    Text(it.title)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchScreen() {
+    Text("Search")
+}
+
+@Composable
+fun SettingsScreen() {
+    Text("Settings")
 }
