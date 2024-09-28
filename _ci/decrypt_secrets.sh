418e79eda354bf90276aaf07f0f5786f3dd6421b
Olivier Patry <olivier.patry@gmail.com>
2024-09-28 09:12:37 +0200

    Add store signing certificate

--- _ci/decrypt_secrets.sh
+++ _ci/decrypt_secrets.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 origin=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit
 
 cd "${origin}"
+./decrypt_file.sh "${origin}/tasksApp.keystore.gpg"
 ./decrypt_file.sh "${origin}/google-services.json.gpg" \
                   "${origin}/../tasks-app-android/google-services.json"
 
