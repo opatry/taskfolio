9da2e2640ebf7843981b91328d811c253ebb78bd
Olivier Patry <olivier.patry@gmail.com>
2024-09-28 09:11:24 +0200

    Align Android UI with desktop

--- tasks-app-android/src/main/java/net/opatry/tasks/app/MainActivity.kt
+++ tasks-app-android/src/main/java/net/opatry/tasks/app/MainActivity.kt
@@ -25,8 +25,24 @@ package net.opatry.tasks.app
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.opatry.tasks.app.ui.TaskListsViewModel
+import net.opatry.tasks.app.ui.TasksApp
+import net.opatry.tasks.app.ui.UserState
+import net.opatry.tasks.app.ui.UserViewModel
+import net.opatry.tasks.app.ui.screen.AuthorizationScreen
 import net.opatry.tasks.app.ui.theme.TasksAppTheme
+import org.koin.compose.viewmodel.koinViewModel
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,8 +50,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val userViewModel = koinViewModel<UserViewModel>()
+            val userState by userViewModel.state.collectAsState(null)
+
+            if (userState == null) {
+                LaunchedEffect(userState) {
+                    userViewModel.refreshUserState()
+                }
+            }
+
             TasksAppTheme {
                 Surface {
+                    when (userState) {
+                        null -> Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            CircularProgressIndicator(Modifier.size(24.dp), strokeWidth = 1.dp)
+                        }
+
+                        is UserState.Unsigned,
+                        is UserState.SignedIn -> {
+                            val tasksViewModel = koinViewModel<TaskListsViewModel>()
+                            TasksApp(userViewModel, tasksViewModel)
+                        }
+
+                        is UserState.Newcomer -> AuthorizationScreen(
+                            onSkip = userViewModel::skipSignIn,
+                            onSuccess = userViewModel::signIn,
+                        )
+                    }
                 }
             }
         }
