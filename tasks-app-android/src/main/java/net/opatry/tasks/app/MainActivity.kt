5523a55de9a18894ed35fa78bec7d948d0264937
Olivier Patry <olivier.patry@gmail.com>
2024-09-30 18:31:47 +0200

    WIP In-app update flow for Android

--- tasks-app-android/src/main/java/net/opatry/tasks/app/MainActivity.kt
+++ tasks-app-android/src/main/java/net/opatry/tasks/app/MainActivity.kt
@@ -22,13 +22,24 @@
 
 package net.opatry.tasks.app
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.common.IntentSenderForResultStarter
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import net.opatry.tasks.app.ui.TaskListsViewModel
 import net.opatry.tasks.app.ui.TasksApp
 import net.opatry.tasks.app.ui.UserState
@@ -44,6 +55,8 @@ import org.koin.compose.viewmodel.koinViewModel
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkStoreUpdateFlow()
 
         setContent {
             val userViewModel = koinViewModel<UserViewModel>()
@@ -80,5 +93,82 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private val updateLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        if (result.data == null) return@registerForActivityResult
+        Toast.makeText(this, "Downloading stated", Toast.LENGTH_SHORT).show()
+        if (result.resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "Downloading failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private val updateResultStarter =
+        IntentSenderForResultStarter { intent, _, fillInIntent, flagsMask, flagsValues, _, _ ->
+            val request = IntentSenderRequest.Builder(intent)
+                .setFillInIntent(fillInIntent)
+                .setFlags(flagsValues, flagsMask)
+                .build()
+            // launch updateLauncher
+            updateLauncher.launch(request)
+        }
+    val listener = InstallStateUpdatedListener { state ->
+        // (Optional) Provide a download progress bar.
+        when (state.installStatus()) {
+            InstallStatus.DOWNLOADING -> {
+                val bytesDownloaded = state.bytesDownloaded()
+                val totalBytesToDownload = state.totalBytesToDownload()
+                // Show update progress bar.
+            }
+
+            InstallStatus.DOWNLOADED -> {
+                Toast.makeText(
+                    this,
+                    "New app is ready",
+                    Toast.LENGTH_SHORT
+                ).show()
+//                    appUpdateManager.completeUpdate()
+            }
+
+            InstallStatus.CANCELED -> Unit
+            InstallStatus.FAILED -> Unit
+            InstallStatus.INSTALLED -> Unit
+            InstallStatus.INSTALLING -> Unit
+            InstallStatus.PENDING -> Unit
+            InstallStatus.REQUIRES_UI_INTENT -> Unit
+            InstallStatus.UNKNOWN -> Unit
+        }
+    }
+
+    private fun checkStoreUpdateFlow() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+        appUpdateManager.registerListener(listener)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+        // TODO appUpdateManager.unregisterListener(listener)
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                when {
+                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+                            && appUpdateInfo.updatePriority() >= 4 -> {
+                        appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            updateResultStarter,
+                            AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build(),
+                            UPDATE_REQUEST_CODE
+                        )
+                    }
+
+                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+                            && appUpdateInfo.clientVersionStalenessDays() ?: -1 >= DAYS_FOR_FLEXIBLE_UPDATE -> {
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val UPDATE_REQUEST_CODE = 1
+        private const val DAYS_FOR_FLEXIBLE_UPDATE = 7
     }
 }
