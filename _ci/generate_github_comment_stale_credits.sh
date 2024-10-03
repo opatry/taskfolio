24bae2b7252d8ef69c25424a51c3c023a65a71ff
Olivier Patry <olivier.patry@gmail.com>
2024-10-03 14:16:29 +0200

    Notify about stale credits with PR comment

--- _ci/generate_github_comment_stale_credits.sh
+++ _ci/generate_github_comment_stale_credits.sh
@@ -2,13 +2,16 @@
 
 set -euo pipefail
 
+# need to make explicit `\n` char line ending to be properly managed by
+# Github action output variable processing
+
 case "$1" in
   ":tasks-app-android")
-  diff=$(git diff tasks-app-android/src/main/assets/licenses_android.json)
+  diff=$(git diff tasks-app-android/src/main/assets/licenses_android.json | awk '{printf "%s\\n", $0}')
   update_cmd="./gradlew :tasks-app-android:exportLibraryDefinitions -PaboutLibraries.exportPath=src/main/assets"
   ;;
   ":tasks-app-desktop")
-  diff=$(git diff tasks-app-desktop/src/main/resources/licenses_desktop.json)
+  diff=$(git diff tasks-app-desktop/src/main/resources/licenses_desktop.json | awk '{printf "%s\\n", $0}')
   update_cmd="./gradlew :tasks-app-desktop:exportLibraryDefinitions -PaboutLibraries.exportPath=src/main/resources"
   ;;
   *)
@@ -18,10 +21,9 @@ case "$1" in
 esac
 
 cat << __END
-## ©️ Stale credits for \`$1\`
-
-\`\`\`diff
-${diff}
-\`\`\`
+## ©️ Stale credits for \`$1\`\n\n
+\`\`\`diff\n
+${diff}\n
+\`\`\`\n\n
 Run \`${update_cmd}\` and commit resulting diff to fix the issue.
 __END
