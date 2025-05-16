c39cb6d14a41db7a2b81cb4882baa8dce73639a8
Olivier Patry <olivier.patry@gmail.com>
2025-05-16 21:20:16 +0200

    WIP TODO tests completion

--- tasks-app-shared/src/commonTest/kotlin/net/opatry/tasks/data/TaskRepositoryCRUDTest.kt
+++ tasks-app-shared/src/commonTest/kotlin/net/opatry/tasks/data/TaskRepositoryCRUDTest.kt
@@ -261,6 +261,12 @@ class TaskRepositoryCRUDTest {
         assertTrue(tasks.first().isCompleted)
     }
 
+    // TODO add test for parent task to complete children
+    // TODO add test for task restoration (1st position)
+    // TODO add test for task restoration (1st position, not restore children)
+    // TODO add test for child task restoration with parent uncompleted (1st position in parent task)
+    // TODO add test for child task restoration with parent completed (1st position in list)
+
     @Test
     fun `toggle unavailable task completion state should throw IllegalArgumentException`() = runTaskRepositoryTest { repository ->
         assertFailsWith<IllegalArgumentException>("Invalid task id 42") {
