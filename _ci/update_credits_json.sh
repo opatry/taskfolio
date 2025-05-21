9ffde08481751fd1f2da0806101f68beb11b62dc
Olivier Patry <olivier.patry@gmail.com>
2025-05-21 18:54:47 +0200

    Simplify update_credits_json.sh now that there isn't module specificities
    Could even be removed, cleaning & calling isn't that complicated.
    Kept as is because not cleaning might be unreliable sometimes.

--- _ci/update_credits_json.sh
+++ _ci/update_credits_json.sh
@@ -4,14 +4,7 @@ set -euo pipefail
 
 origin=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) || exit
 
-# shellcheck disable=SC1091
-. "${origin}/utils.sh"
-
 cd "${origin}/.."
 
-for module in "tasks-app-desktop" "tasks-app-android"; do
-  step "Updating credits for ${GREEN_BOLD}:${module}${RESET}"
-  find "${module}" -name "licenses*.json" -delete
-  ./gradlew ":${module}:exportLibraryDefinitions"
-  step_done
-done
+find . -name "licenses*.json" -delete
+./gradlew "exportLibraryDefinitions"
