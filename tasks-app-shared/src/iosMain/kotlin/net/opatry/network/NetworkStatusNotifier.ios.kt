268e6094e3ea79e7f7e3e1c5d135737b45fec1a3
Olivier Patry <olivier.patry@gmail.com>
2025-06-21 01:11:25 +0200

    WIP Implement networkStateFlow for iOS

--- tasks-app-shared/src/iosMain/kotlin/net/opatry/network/NetworkStatusNotifier.ios.kt
+++ tasks-app-shared/src/iosMain/kotlin/net/opatry/network/NetworkStatusNotifier.ios.kt
@@ -22,9 +22,32 @@
 
 package net.opatry.network
 
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import platform.Network.nw_path_get_status
+import platform.Network.nw_path_monitor_cancel
+import platform.Network.nw_path_monitor_create
+import platform.Network.nw_path_monitor_set_queue
+import platform.Network.nw_path_monitor_set_update_handler
+import platform.Network.nw_path_monitor_start
+import platform.Network.nw_path_status_satisfied
+import platform.darwin.dispatch_queue_create
 
-actual fun networkStateFlow(): Flow<Boolean> {
-    return flowOf(false)
-}
+actual fun networkStateFlow(): Flow<Boolean> = callbackFlow {
+    val monitor = nw_path_monitor_create()
+    val queue = dispatch_queue_create("NetworkMonitorQueue", null)
+
+    nw_path_monitor_set_update_handler(monitor) { path ->
+        val hasInternet = nw_path_get_status(path) == nw_path_status_satisfied
+        trySend(hasInternet).isSuccess
+    }
+
+    nw_path_monitor_set_queue(monitor, queue)
+    nw_path_monitor_start(monitor)
+
+    awaitClose {
+        nw_path_monitor_cancel(monitor)
+    }
+}.distinctUntilChanged()
