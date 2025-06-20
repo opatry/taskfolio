d209facb20a32d7e3427de5c8f61a48cb8a9f36d
Olivier Patry <olivier.patry@gmail.com>
2025-06-21 01:55:49 +0200

    Very quick and VERYYY dirty fake token sync from desktop app to iOS sim

--- tasks-app-shared/src/iosMain/kotlin/net/opatry/tasks/app/di/authModule.ios.kt
+++ tasks-app-shared/src/iosMain/kotlin/net/opatry/tasks/app/di/authModule.ios.kt
@@ -22,10 +22,24 @@
 
 package net.opatry.tasks.app.di
 
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.datetime.Clock
+import kotlinx.serialization.json.Json
 import net.opatry.google.auth.GoogleAuthenticator
 import net.opatry.google.auth.GoogleAuthenticator.OAuthToken.TokenType
+import net.opatry.tasks.TokenCache
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import platform.Foundation.NSData
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSString
+import platform.Foundation.NSUTF8StringEncoding
+import platform.Foundation.NSUserDomainMask
+import platform.Foundation.create
+import platform.Foundation.dataWithContentsOfFile
+import kotlin.time.Duration.Companion.milliseconds
 
 actual fun authModule(gcpClientId: String): Module = module {
     single<GoogleAuthenticator> {
@@ -37,14 +51,43 @@ actual fun authModule(gcpClientId: String): Module = module {
                 requestUserAuthorization: (Any) -> Unit
             ): String = ""
 
-            override suspend fun getToken(grant: GoogleAuthenticator.Grant) = GoogleAuthenticator.OAuthToken(
-                accessToken = "",
-                expiresIn = 0L,
-                idToken = null,
-                refreshToken = null,
-                scope = "",
-                tokenType = TokenType.Bearer,
+            @OptIn(
+                BetaInteropApi::class,
+                ExperimentalForeignApi::class,
             )
+            override suspend fun getToken(grant: GoogleAuthenticator.Grant): GoogleAuthenticator.OAuthToken {
+                val now = Clock.System.now().toEpochMilliseconds()
+                val fileManager = NSFileManager.defaultManager
+                val documentDirectoryPath = fileManager.URLForDirectory(
+                    directory = NSDocumentDirectory,
+                    inDomain = NSUserDomainMask,
+                    appropriateForURL = null,
+                    create = true,
+                    error = null,
+                )?.path ?: throw IllegalStateException("Could not find document directory")
+                val credentialsFilePath = "$documentDirectoryPath/hacky_token_cache.json"
+
+                require(fileManager.fileExistsAtPath(credentialsFilePath)) {
+                    "Credentials file does not exist at path: $credentialsFilePath"
+                }
+
+                val data = NSData.dataWithContentsOfFile(credentialsFilePath)
+                    ?: error("Failed to load data from $credentialsFilePath")
+
+                val content = NSString.create(data, NSUTF8StringEncoding)?.toString()
+                    ?: error("Failed to convert data to string from $credentialsFilePath")
+
+                return Json.decodeFromString<TokenCache>(content).let {
+                    GoogleAuthenticator.OAuthToken(
+                        accessToken = it.accessToken ?: "",
+                        expiresIn = (it.expirationTimeMillis - now).milliseconds.inWholeSeconds,
+                        idToken = null,
+                        refreshToken = it.refreshToken,
+                        scope = "", // TODO
+                        tokenType = TokenType.Bearer,
+                    )
+                }
+            }
         }
     }
 }
--- tasks-app-shared/src/iosMain/kotlin/net/opatry/tasks/app/ui/component/AuthorizeGoogleTasksButton.ios.kt
+++ tasks-app-shared/src/iosMain/kotlin/net/opatry/tasks/app/ui/component/AuthorizeGoogleTasksButton.ios.kt
@@ -22,19 +22,79 @@
 
 package net.opatry.tasks.app.ui.component
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import net.opatry.google.auth.GoogleAuthenticator
+import net.opatry.google.tasks.TasksScopes
+import net.opatry.tasks.resources.Res
+import net.opatry.tasks.resources.onboarding_screen_authorize_cta
+import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
 
 @Composable
 actual fun AuthorizeGoogleTasksButton(
     modifier: Modifier,
     onSuccess: (GoogleAuthenticator.OAuthToken) -> Unit
 ) {
-    // TODO
-    Button(onClick = {}) {
-        Text("TODO Google Authorization")
+    val uriHandler = LocalUriHandler.current
+    val coroutineScope = rememberCoroutineScope()
+    val authenticator = koinInject<GoogleAuthenticator>()
+    var ongoingAuth by remember { mutableStateOf(false) }
+    var error by remember { mutableStateOf<String?>(null) }
+
+    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Button(
+            onClick = {
+                ongoingAuth = true
+                coroutineScope.launch {
+                    val scope = listOf(
+                        GoogleAuthenticator.Scope.Profile,
+                        GoogleAuthenticator.Scope(TasksScopes.Tasks),
+                    )
+                    try {
+                        val authCode = authenticator.authorize(scope, true) {
+                            // TODO deal with iOS
+                        }.let(GoogleAuthenticator.Grant::AuthorizationCode)
+                        val oauthToken = authenticator.getToken(authCode)
+                        onSuccess(oauthToken)
+                    } catch (e: Exception) {
+                        error = e.message
+                        ongoingAuth = false
+                    }
+                }
+            },
+            enabled = !ongoingAuth
+        ) {
+            Box(modifier, contentAlignment = Alignment.Center) {
+                AnimatedContent(ongoingAuth, label = "authorize_button_content") { ongoing ->
+                    if (ongoing) {
+                        LoadingIndicator(Modifier.size(24.dp))
+                    } else {
+                        Text(stringResource(Res.string.onboarding_screen_authorize_cta))
+                    }
+                }
+            }
+        }
+
+        AnimatedContent(error, label = "authorize_error_message") { message ->
+            Text(message ?: "", color = MaterialTheme.colorScheme.error)
+        }
     }
 }
