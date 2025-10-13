a0f269ee357de4cdcaff80c057f78358e6ef896c
Olivier Patry <olivier.patry@gmail.com>
2025-10-13 19:44:33 +0200

    Fix about app icon size

--- tasks-app-shared/src/commonMain/kotlin/net/opatry/tasks/app/ui/screen/aboutScreen.kt
+++ tasks-app-shared/src/commonMain/kotlin/net/opatry/tasks/app/ui/screen/aboutScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -173,7 +174,7 @@ private fun AppIcon() {
     Box(
         Modifier
             .padding(8.dp)
-            .fillMaxHeight()
+            .heightIn(min = 24.dp, max = 48.dp)
             .aspectRatio(1f)
             .clip(MaterialTheme.shapes.medium)
             .background(Color(0xFF023232)),
